8e0c7f57a08f172a021e69594eb8c444c7bcb8ef
Nathan Fuller <nathanernestfuller@gmail.com>
2022-02-25 12:24:40 -0500

    Initial Commit

---
+++ WordleAggregator/WordleAggregatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleAggregatorApp.swift
+//  WordleAggregator
+//
+//  Created by Nathan Fuller on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleAggregatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
