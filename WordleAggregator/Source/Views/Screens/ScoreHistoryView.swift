4b5a73d183b7ea76ad4594e7d1a172f467530373
Nathan Fuller <nathanernestfuller@gmail.com>
2022-04-02 19:19:55 -0400

    Hook up ScoreHistoryView to managed object context

--- WordleAggregator/Source/Views/Screens/ScoreHistoryView.swift
+++ WordleAggregator/Source/Views/Screens/ScoreHistoryView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct ScoreHistoryView: View {
+
+  @FetchRequest(
+    entity: Score.entity(),
+    sortDescriptors: [
+      NSSortDescriptor(keyPath: \Score.date, ascending: true)
+    ]
+  ) var scores: FetchedResults<Score>
+
+  @State private var isErrorState: Bool = false
+  @State private var errorMessage: String? { didSet { isErrorState = !errorMessage.isNilOrEmpty }}
+
   var body: some View {
     List {
-      Text("hi")
-      Text("hi again")
-    }.navigationTitle("score history")
+      ForEach(scores, id: \.self) { score in
+        HStack {
+          Text(score.title ?? "")
+          Text(score.answer ?? "")
+          Text("\(score.numberOfGuesses)/\(score.maxGuesses)")
+        }
+      }
+      .onDelete(perform: removeScore)
+    }
+    .alert("Something went wrong", isPresented: $isErrorState, actions: {
+      Button("Okay", role: .cancel, action: { errorMessage = nil })
+    }, message: {
+      Text(errorMessage ?? "")
+    })
+    .listRowSeparator(.hidden)
+    .navigationTitle("score history")
+  }
+
+  func removeScore(at offsets: IndexSet) {
+    for index in offsets {
+      let score = scores[index]
+      CoreDataStack.context.delete(score)
+    }
+
+    do {
+      try CoreDataStack.saveContext()
+    } catch {
+      CoreDataStack.context.rollback()
+
+#if DEBUG
+      errorMessage = error.localizedDescription
+#else
+      errorMessage = "There was a problem saving your score. The developer has been notified of this issue ✍️"
+#endif
+    }
   }
 }
 
