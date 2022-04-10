cc5dc319604a86ca093b9952852ea3ea8b70b793
Nathan Fuller <nathanernestfuller@gmail.com>
2022-04-10 10:28:38 -0400

    WV-2 Firebase setup

--- WordleVerse/Source/WordleVerseApp.swift
+++ WordleVerse/Source/WordleVerseApp.swift
@@ -6,10 +6,23 @@
 //
 
 import CoreData
+import Firebase
 import SwiftUI
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct WordleVerseApp: App {
+
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
   var body: some Scene {
     WindowGroup {
       HomeScreenView()
